0275e4e9488565c51d3cd396262164246192d2dc
MossPipat <pt_moss17782@hotmail.com>
2024-02-13 00:16:32 +0700

    [feature]: setup react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./styles/theme";
 import { ExploreMedia } from "./pages/ExploreMediaPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <ExploreMedia />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ExploreMedia />} />
+          <Route path="/explore" element={<ExploreMedia />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
